a8f60ed4e6f307b8245dcc02152fb7dc07760c09
semnil5202 <semnil5202@gmail.com>
2024-01-09 07:45:28 +0900

    design: 에러메세지가 없을 경우 에러메세지 공간을 제거하도록 변경

--- src/components/Field/FieldInput.tsx
+++ src/components/Field/FieldInput.tsx
@@ -47,13 +47,18 @@ const FieldInput = ({
         maxLength={maxLength}
         errorMessage={errorMessage}
       />
-      <MessageWrapper>
-        {!errorMessage && isSuccess && value ? (
-          <SuccessMessageWrapper>{successMessage}</SuccessMessageWrapper>
-        ) : (
-          <ErrorMessageWrapper>{errorMessage}</ErrorMessageWrapper>
-        )}
-      </MessageWrapper>
+
+      {!errorMessage && isSuccess && value && (
+        <MessageWrapper isNonError={!errorMessage && isSuccess}>
+          {successMessage}
+        </MessageWrapper>
+      )}
+
+      {(errorMessage || !isSuccess) && (
+        <MessageWrapper isNonError={!errorMessage && isSuccess}>
+          {errorMessage}
+        </MessageWrapper>
+      )}
     </>
   );
 };
@@ -66,8 +71,8 @@ const Input = styled.input<{ errorMessage: string }>`
   height: 44px;
   border-radius: 6px;
   padding: 11px 16px;
-  font-size: 14px;
-  font-weight: 400;
+  font-size: ${({ theme }) => theme.font.suit14r.fontSize}px;
+  font-weight: ${({ theme }) => theme.font.suit14r.fontWeight};
   border: 1px solid
     ${({ errorMessage, theme }) =>
       errorMessage ? theme.color.c3 : theme.color.l2};
@@ -80,18 +85,12 @@ const Input = styled.input<{ errorMessage: string }>`
   }
 `;
 
-const MessageWrapper = styled.div`
+const MessageWrapper = styled.p<{ isNonError: boolean }>`
   margin-top: 12px;
   font-size: 12px;
   font-weight: 400;
   width: 100%;
   height: 15px;
-`;
-
-const ErrorMessageWrapper = styled.p`
-  color: ${({ theme }) => theme.color.c3};
-`;
-
-const SuccessMessageWrapper = styled.p`
-  color: ${({ theme }) => theme.color.c2};
+  color: ${({ theme, isNonError }) =>
+    isNonError ? theme.color.c2 : theme.color.c3};
 `;
