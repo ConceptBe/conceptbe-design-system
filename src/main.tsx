e5e8de1823086f7e52fba245ed6dbb6efa79b6a0
semnil5202 <semnil5202@gmail.com>
2024-01-09 06:51:30 +0900

    chore: 테스트환경 스타일링 통일을 위한 reset.css 적용

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 
 import { theme } from '.';
 import App from './App';
+import './styles/reset.css';
 
 createRoot(document.getElementById('root')!).render(
   <ThemeProvider theme={theme}>
