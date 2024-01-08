67b2c46fb0af391546fc2962e3d3754dcdf798e1
semnil5202 <semnil5202@gmail.com>
2024-01-09 06:38:42 +0900

    refactor: 불필요한 DropdownItem interface 수정

--- src/components/Dropdown/DropdownItem.tsx
+++ src/components/Dropdown/DropdownItem.tsx
@@ -4,36 +4,25 @@ import { useContext } from 'react';
 import { DropdownContext } from './Dropdown';
 
 export interface PanelProps {
-  optionId: number;
   value: string;
   children: string;
   onClick: (value: string) => void;
 }
 
-const DropdownItem = ({ optionId, children, value, onClick }: PanelProps) => {
+const DropdownItem = ({ children, value, onClick }: PanelProps) => {
   const { onClickDisActive } = useContext(DropdownContext);
 
-  const onClickItem = (clickedValue: string) => {
-    onClick(clickedValue);
+  const onClickItem = () => {
+    onClick(value);
     onClickDisActive();
   };
 
-  return (
-    <Wrapper
-      optionId={optionId}
-      value={value}
-      onClick={() => {
-        optionId === 0 ? null : onClickItem(value);
-      }}
-    >
-      {children}
-    </Wrapper>
-  );
+  return <Wrapper onClick={onClickItem}>{children}</Wrapper>;
 };
 
 export default DropdownItem;
 
-const Wrapper = styled.li<{ optionId: number }>`
+const Wrapper = styled.li`
   display: flex;
   justify-content: flex-start;
   align-items: center;
@@ -41,9 +30,8 @@ const Wrapper = styled.li<{ optionId: number }>`
   padding: 11px 15px 12px;
   box-sizing: border-box;
   border-bottom: 1px solid ${({ theme }) => theme.color.l3};
-  cursor: ${({ optionId }) => (optionId === 0 ? 'auto' : 'pointer')};
-  color: ${({ theme, optionId }) =>
-    optionId === 0 ? theme.color.b9 : theme.color.b6};
+  cursor: 'pointer';
+  color: ${({ theme }) => theme.color.b6};
   font-size: 12px;
   font-weight: 400;
 
