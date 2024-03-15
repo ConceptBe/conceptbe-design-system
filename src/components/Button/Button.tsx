54ce1a893e181574c76863708f8ea7493a6729fa
semnil5202 <semnil5202@gmail.com>
2024-03-15 23:54:35 +0900

    refactor: Button 컴포넌트 Emotion 다형성 컴포넌트 방식으로 변경 및 속성 추가

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,49 +1,48 @@
 import styled from '@emotion/styled';
-import {
-  ComponentPropsWithoutRef,
-  ReactNode,
-  ElementType,
-  MouseEventHandler,
-} from 'react';
 
-type Props<T extends ElementType> = {
-  children: ReactNode;
-  as?: T;
-  onClick?: MouseEventHandler<HTMLButtonElement>;
+import { convertCSS } from '../..';
+
+export interface Props {
+  width?: number | string;
+  height?: number | string;
+  minWidth?: number | string;
+  minHeight?: number | string;
+  maxWidth?: number | string;
+  maxHeight?: number | string;
+  padding?: string;
+  paddingTop?: number | string;
+  paddingRight?: number | string;
+  paddingBottom?: number | string;
+  paddingLeft?: number | string;
   isGrayOut?: boolean;
-} & ComponentPropsWithoutRef<T>;
+}
 
-const Button = <T extends ElementType>({
-  as,
-  onClick = () => {},
-  isGrayOut = false,
-  children,
-  ...attributes
-}: Props<T>) => {
-  const tag = as || 'button';
-
-  return (
-    <Wrapper as={tag} onClick={onClick} isGrayOut={isGrayOut} {...attributes}>
-      {children}
-    </Wrapper>
-  );
-};
-
-export default Button;
-
-const Wrapper = styled.button<{ isGrayOut: boolean }>`
+const Button = styled.button<Props>`
   display: flex;
   justify-content: center;
   align-items: center;
+  border: none;
+  border-radius: 6px;
+  cursor: pointer;
+  width: ${({ width }) => (width ? convertCSS(width) : '100%')};
+  height: ${({ height }) => height && convertCSS(height)};
+  min-width: ${({ minWidth }) => minWidth && convertCSS(minWidth)};
+  min-height: ${({ minHeight }) => minHeight && convertCSS(minHeight)};
+  max-width: ${({ maxWidth }) => maxWidth && convertCSS(maxWidth)};
+  max-height: ${({ maxHeight }) => maxHeight && convertCSS(maxHeight)};
   background-color: ${({ isGrayOut, theme }) =>
     isGrayOut ? theme.color.bg1 : theme.color.c1};
   color: ${({ isGrayOut, theme }) =>
     isGrayOut ? theme.color.b : theme.color.w1};
-  border: none;
-  border-radius: 6px;
   font-size: ${({ theme }) => theme.font.suit16sb.fontSize}px;
   font-weight: ${({ theme }) => theme.font.suit16sb.fontWeight};
-  width: 100%;
-  padding: 17px 28px;
-  cursor: pointer;
+  padding: ${({ padding }) => (padding ? padding : '17px 28px')};
+  padding-top: ${({ paddingTop }) => paddingTop && convertCSS(paddingTop)};
+  padding-right: ${({ paddingRight }) =>
+    paddingRight && convertCSS(paddingRight)};
+  padding-bottom: ${({ paddingBottom }) =>
+    paddingBottom && convertCSS(paddingBottom)};
+  padding-left: ${({ paddingLeft }) => paddingLeft && convertCSS(paddingLeft)};
 `;
+
+export default Button;
